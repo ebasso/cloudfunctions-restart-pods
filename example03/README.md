140a729309095e8d2c8748dafb2bb0ba6955d575
Enio Basso <ebasso@ebasso.net>
2020-05-05 18:52:05 -0300

    update 2020-05-05-02

--- example03/README.md
+++ example03/README.md
@@ -1,113 +1,51 @@
-Example 03 - Using IBM Cloud Functions to Restart Pods on Openshift
+Example 03 - List and Delete pods on OpenShift
 ===========================
 
+In below examples, we will deploy IBM Cloud Functions using a manifest.yaml file.
 
-#### Preparing environment:
+Sample manifest.yaml file
 
-```bash
-cd ibm-cloud-functions-examples/example03/install_dir
-wget https://mirror.openshift.com/pub/openshift-v4/clients/oc/latest/linux/oc.tar.gz
+```yaml
+packages:
+  OpenshiftFnPkgTools:
+    version: 1.0
+    license: Apache-2.0
+    actions:
+      list-pods-in-namespace:
+        function: list-pods.py
+        inputs:
+          IBMCLOUD_OC_CONSOLE: https://c101-e.us-south.containers.cloud.ibm.com:NNNNN
+          IBMCLOUD_OC_TOKEN: XXXXXX-XXXXX-XXXXXX
+          IBMCLOUD_OC_PROJECT: YYYYYY
+          APIKEY: ZZZZZ
+      delete-pods-in-namespace:
+        function: delete-pods.py
+        inputs:
+          IBMCLOUD_OC_CONSOLE: https://c101-e.us-south.containers.cloud.ibm.com:NNNNN
+          IBMCLOUD_OC_TOKEN: XXXXXX-XXXXX-XXXXXX
+          IBMCLOUD_OC_PROJECT: YYYYYY
+          APIKEY: ZZZZZ
+```
+The deployment manifest file defines the following variables.
 
-cd ..
-chmod a+x ibmfnc.sh
+* The package name.
+* The action name.
+* The action code file name.
+* The action parameters
+
+Create action using manifest.yaml
+
+```sh
+$ cd example03
+
+$ ibmcloud fn deploy --manifest manifest.yaml
+
+Success: Deployment completed successfully.
 ```
 
-Defining necessary environment variables
+Change variables on IBM Cloud Console
 
-```bash
-export DOCKER_MAINTAINER="<YOUR_EMAIL_ADDRESS>"
-export DOCKERHUB_USERNAME="<YOUR_USER_ON_DOCKER_HUB>"
-```
-
-Build your custom Docker image
-
-```bash
-./ibmfnc.sh build
-```
-
-Checking my image
-
-```bash
-$ docker images
-
-REPOSITORY                                      TAG                 IMAGE ID            CREATED             SIZE
-<YOUR_USER_ON_DOCKER_HUB>/restart-oc-pods       latest              c07065f08e57        6 seconds ago       301MB
-```
-
-#### Running Container
-
-Defining necessary environment variables to test:
-
-```bash
-export IBMCLOUD_OC_TOKEN=""
-export IBMCLOUD_OC_CONSOLE="https://cNNN-e.us-south.containers.cloud.ibm.com:NNNNN"
-export IBMCLOUD_OC_PROJECT=""
-export SLEEP_TIME="5"
-```
-
-where:
 
 * IBMCLOUD_OC_TOKEN:  token to authenticate against OpenShift on IBM Cloud.
 * IBMCLOUD_OC_CONSOLE:  url to OpenShift on IBM Cloud.
 * IBMCLOUD_OC_PROJECT:  need to define project on OpenShift.
-
-Running the script
-
-```bash
-./ibmfnc.sh run
-```
-
-#### Running container manually
-
-```bash
-docker run -e IBMCLOUD_OC_TOKEN -e IBMCLOUD_OC_CONSOLE -e IBMCLOUD_OC_PROJECT $DOCKERHUB_USERNAME/restart-oc-pods
-```
-
-
-
-# Upload Container to Docker Hub
-
-1. Login on Docker Hub
-
-```bash
-docker login 
-```
-
-2. Push image
-
-```bash
-docker push $DOCKERHUB_USERNAME/restart-oc-pods
-```
-
-# Deploying an action with a custom Docker image
-
-using manifest.yaml file
-```bash
-$ ibmcloud fn deploy --manifest manifest.yaml
-
-ok: created action example03/restart-oc-pods
-```
-or manually
-
-```bash
-$ ibmcloud fn package create example03
-
-$ ibmcloud fn action create example03/restart-oc-pods --docker $DOCKERHUB_USERNAME/restart-oc-pods
-
-ok: created action example03/restart-oc-pods
-```
-
-Run and get logs
-
-```bash
-$ ibmcloud fn action invoke example03/restart-oc-pods --result
-
-ok: invoked /_/example03/restart-oc-pods, but the request has not yet finished, with id <ACTIVATION_ID>
-
-$ ibmcloud fn activation get <ACTIVATION_ID>
-```
-
-
-Looking at IBM Cloud console, you can see the new created Cloud Function
-
-![Screen capture of the Cloud Function](../readme_images/example03-01.png)
---
+++ example03/list-pods.py
@@ -0,0 +1,105 @@
+import requests, json
+from requests.auth import HTTPBasicAuth
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+# Disable Warnings from Untrusted TLS keys
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+class App(object):
+    
+    def __init__(self, url, apikey,token):
+        self.url = url
+        self.apikey = apikey
+        self.token = token
+        self.iam_access_token = None
+        
+        
+    def __doPost(self, url, data, headers=None, auth=None):
+        try:
+            r = requests.post(url=url, headers=None,  data=data, auth=auth, verify=False)
+
+            if (r.status_code != 200):
+                print('requests.get -> %s = %s\n' % (r.url, r))
+                return None
+            return r.content
+        except requests.exceptions.RequestException as e:
+            print(url, e)
+            return None
+
+    def __doGet(self, url, data, headers=None, auth=None):
+
+        try:
+            r = requests.get(url=url, headers=None, auth=auth, verify=False)
+            #print('doGet')
+            print(r)
+
+            if (r.status_code != 200):
+                print('requests.get -> %s = %s\n' % (r.url, r))
+                return None
+            return r.content
+        except requests.exceptions.RequestException as e:
+            print(url, e)
+            return None
+
+    def getIAMToken(self):
+        url = 'https://iam.cloud.ibm.com/identity/token'
+        headers = { 'Content-Type': 'application/x-www-form-urlencoded', 'Accept': 'application/json'}
+        form =  { 'grant_type': 'urn:ibm:params:oauth:grant-type:apikey',
+                  'apikey': self.apikey}
+
+        r = requests.post(url, headers=headers,  data=form, verify=False) .json()
+        #print(r)
+
+        if ( r['access_token'] ):
+            self.iam_access_token = r['access_token']
+
+    def getAllPodsInNamespace(self,namespace):
+        '''
+        curl -k -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' \
+            https://$ENDPOINT/api/v1/namespaces/$NAMESPACE/pods
+        '''
+        #authorization = 'Bearer %s' % self.iam_access_token
+        authorization = 'Bearer %s' % self.token
+        headers = {'Content-type': 'application/json', 'Accept': 'application/json', 'Authorization' : authorization }
+        url=self.url + '/api/v1/namespaces/' + namespace + '/pods'
+
+        #print('url='+ url )
+        r = requests.get(url=url, headers=headers, verify=False)
+        #print ('Response: ',r.status_code, ' - ', r.json())
+        #print (r.json())
+        return r.json()
+
+    def deleteAllPodsInNamespace(self,namespace):
+        '''
+        curl -k -X DELETE -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' \
+            https://$ENDPOINT/api/v1/namespaces/$NAMESPACE/pods
+        '''
+        #authorization = 'Bearer %s' % self.iam_access_token
+        authorization = 'Bearer %s' % self.token
+        headers = {'Content-type': 'application/json', 'Accept': 'application/json', 'Authorization' : authorization }
+        url=self.url + '/api/v1/namespaces/' + namespace + '/pods'
+
+        #print('url='+ url )
+        r = requests.delete(url=url, headers=headers, verify=False)
+        print ('Response: ',r.status_code, ' - ', r.json())
+        
+        
+def main(params):
+
+    app = App(params['IBMCLOUD_OC_CONSOLE'],params['APIKEY'],params['IBMCLOUD_OC_TOKEN'])
+
+    app.getIAMToken()
+    r = app.getAllPodsInNamespace(params['IBMCLOUD_OC_PROJECT'])
+
+    for pod in r['items']:
+        print(pod['metadata']['name'])
+    #app.deleteAllPodsInNamespace(IBMCLOUD_OC_PROJECT)
+
+    return { }
+
+'''
+if __name__ == '__main__':
+    params = {}
+    ret = main(params)
+    #print (ret)
+'''
